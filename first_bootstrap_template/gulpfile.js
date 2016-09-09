f5b059f4ee4049ab374842d2626f365e53f867c7
burks20 <burks20@users.noreply.github.com>
2016-09-09 11:31:46 -0400

    Uploaded "Grayscale" Bootstrap Theme

--- first_bootstrap_template/gulpfile.js
+++ first_bootstrap_template/gulpfile.js
@@ -18,7 +18,7 @@ var banner = ['/*!\n',
 
 // Compile LESS files from /less into /css
 gulp.task('less', function() {
-    return gulp.src('less/grayscale.less')
+    return gulp.src('less/new-age.less')
         .pipe(less())
         .pipe(header(banner, { pkg: pkg }))
         .pipe(gulp.dest('css'))
@@ -29,7 +29,7 @@ gulp.task('less', function() {
 
 // Minify compiled CSS
 gulp.task('minify-css', ['less'], function() {
-    return gulp.src('css/grayscale.css')
+    return gulp.src('css/new-age.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('css'))
@@ -40,7 +40,7 @@ gulp.task('minify-css', ['less'], function() {
 
 // Minify JS
 gulp.task('minify-js', function() {
-    return gulp.src('js/grayscale.js')
+    return gulp.src('js/new-age.js')
         .pipe(uglify())
         .pipe(header(banner, { pkg: pkg }))
         .pipe(rename({ suffix: '.min' }))
@@ -57,6 +57,10 @@ gulp.task('copy', function() {
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
         .pipe(gulp.dest('vendor/jquery'))
+
+    gulp.src(['node_modules/simple-line-icons/*/*'])
+        .pipe(gulp.dest('vendor/simple-line-icons'))
+
 
     gulp.src([
             'node_modules/font-awesome/**',
